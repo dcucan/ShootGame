acb8c05338538d4274714df09983b45fbfc84276
Adam Jedlička <adajedlicka@gmail.com>
2019-11-02 12:42:15 +0100

    Add hello messages

--- src/main/java/shoot/client/Main.java
+++ src/main/java/shoot/client/Main.java
@@ -1,4 +1,7 @@
 package shoot.client;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Client!");
+    }
 }
--- src/main/java/shoot/server/Main.java
+++ src/main/java/shoot/server/Main.java
@@ -5,9 +5,9 @@ import jeda00.db.Migrations;
 public class Main {
 
     public static void main(String[] args) {
-
         Migrations migrations = new Migrations(Database.getConnection());
-
         migrations.runMigrations();
+
+        System.out.println("Hello, Server!");
     }
 }
